3123dd65e92217409857ae7db994cfda5851ff06
Evie Ma <yfma89@gmail.com>
2018-12-10 17:32:43 +1100

    set up basic graphql schemas and resolvers

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("hello world");
-});
+app.use(
+  "/graphql",
+  graphqlHttp({
+    // [String!]!
+    // 1st exclamation mark: MUST be a list of type String
+    // 2nd                  : can't return a list of nulls or just null
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!      
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Romantic Cooking", "Sailing", "All-Night Coding"];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000);
